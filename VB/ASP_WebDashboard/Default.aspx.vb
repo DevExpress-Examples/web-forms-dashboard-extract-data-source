f5e46e2c4a34ff2145fa8d86f26aa3e7609420d6
nakhankov sergey <sergey.nakhankov@devexpress.com>
2019-10-23 16:49:44 +0300

    fix build

--- VB/ASP_WebDashboard/Default.aspx.vb
+++ VB/ASP_WebDashboard/Default.aspx.vb
@@ -14,50 +14,55 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Namespace ASP_WebDashboard
-	Partial Public Class [Default]
-		Inherits System.Web.UI.Page
+    Partial Public Class [Default]
+        Inherits System.Web.UI.Page
 
-		Private Shared extractFileName As String = HostingEnvironment.MapPath("~/App_Data/ExtractDataSource/") & "ExtractDS.dat"
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			Dim dataSourceStorage As New DataSourceInMemoryStorage()
-			dataSourceStorage.RegisterDataSource("extractDataSource", CreateExtractDataSource().SaveToXml())
-			ASPxDashboard1.SetDataSourceStorage(dataSourceStorage)
+        Private Shared extractFileName As String = HostingEnvironment.MapPath("~/App_Data/ExtractDataSource/") & "ExtractDS.dat"
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim dataSourceStorage As New DataSourceInMemoryStorage()
+            dataSourceStorage.RegisterDataSource("extractDataSource", CreateExtractDataSource().SaveToXml())
+            ASPxDashboard1.SetDataSourceStorage(dataSourceStorage)
 
-			If (Not File.Exists(extractFileName)) Then
-				Using ds = CreateExtractDataSource()
-					ds.UpdateExtractFile()
-				End Using
-			End If
-		End Sub
+            If (Not File.Exists(extractFileName)) Then
+                Using ds = CreateExtractDataSource()
+                    ds.UpdateExtractFile()
+                End Using
+            End If
+        End Sub
 
-		Protected Sub ASPxDashboard1_ConfigureDataConnection(ByVal sender As Object, ByVal e As ConfigureDataConnectionWebEventArgs)
-			Dim extractCP As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
-			If extractCP IsNot Nothing Then
-				extractCP.FileName = extractFileName
-			End If
-		End Sub
+        Protected Sub ASPxDashboard1_ConfigureDataConnection(ByVal sender As Object, ByVal e As ConfigureDataConnectionWebEventArgs)
+            Dim extractCP As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
+            If extractCP IsNot Nothing Then
+                extractCP.FileName = extractFileName
+            End If
+        End Sub
 
-		Private Shared Function CreateExtractDataSource() As DashboardExtractDataSource
-			Dim nwindDataSource As New DashboardSqlDataSource("Northwind Invoices", "nwindConnection")
-			Dim invoicesQuery As SelectQuery = SelectQueryFluentBuilder.AddTable("Invoices").SelectColumns("City", "Country", "Salesperson", "OrderDate", "Shippers.CompanyName", "ProductName", "UnitPrice", "Quantity", "Discount", "ExtendedPrice", "Freight").Build("Invoices")
-			nwindDataSource.Queries.Add(invoicesQuery)
-			nwindDataSource.ConnectionOptions.CommandTimeout = 600
+        Private Shared Function CreateExtractDataSource() As DashboardExtractDataSource
+            Dim nwindDataSource As New DashboardSqlDataSource("Northwind Invoices", "nwindConnection")
+            Dim invoicesQuery As SelectQuery = SelectQueryFluentBuilder.AddTable("Invoices").SelectColumns("City", "Country", "Salesperson", "OrderDate", "Shippers.CompanyName", "ProductName", "UnitPrice", "Quantity", "Discount", "ExtendedPrice", "Freight").Build("Invoices")
+            nwindDataSource.Queries.Add(invoicesQuery)
+            nwindDataSource.ConnectionOptions.CommandTimeout = 600
 
-			Dim extractDataSource As New DashboardExtractDataSource("Invoices Extract Data Source")
-			extractDataSource.ExtractSourceOptions.DataSource = nwindDataSource
-			extractDataSource.ExtractSourceOptions.DataMember = "Invoices"
-			extractDataSource.FileName = extractFileName
+            Dim extractDataSource As New DashboardExtractDataSource("Invoices Extract Data Source")
+            extractDataSource.ExtractSourceOptions.DataSource = nwindDataSource
+            extractDataSource.ExtractSourceOptions.DataMember = "Invoices"
+            extractDataSource.FileName = extractFileName
 
-			Return extractDataSource
-		End Function
+            Return extractDataSource
+        End Function
 
-		<WebMethod> _
-		Public Shared Sub UpdateExtractDataSource()
-			Dim ds As DashboardExtractDataSource = CreateExtractDataSource()
-			Dim mre As New ManualResetEvent(False)
-			DashboardExtractDataSource.UpdateFile(ds, Function(a, b) { mre.Set(); }, Function(a, b) { })
-			' Wait until data is refreshed in Extract Data Source
-			mre.WaitOne()
-		End Sub
-	End Class
+        <WebMethod>
+        Public Shared Sub UpdateExtractDataSource()
+            Dim ds As DashboardExtractDataSource = CreateExtractDataSource()
+            Dim mre As New ManualResetEvent(False)
+            DashboardExtractDataSource.UpdateFile(ds,
+                Sub(a, b)
+                    mre.Set()
+                End Sub,
+                Sub(a, b)
+                End Sub)
+            ' Wait until data is refreshed in Extract Data Source
+            mre.WaitOne()
+        End Sub
+    End Class
 End Namespace
